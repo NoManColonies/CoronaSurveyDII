736b4627d3cd893d2b4871f8e53cebceb29c476d
NoManColonies <38200442+NoManColonies@users.noreply.github.com>
2020-06-17 00:26:34 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $password = "";
 $database = "";
 $connect = new mysqli($server, $user, $password, $database);
 if ($connect->connect_errno) {
-  alert_message("Connection timed out for user : ".$user.". error code : ".$connect->errno);
+  echo "Connection timed out for user : ".$user.". error code : ".$connect->errno;
   exit();
 }
 session_start();
