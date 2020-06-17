b5e7459a7a931b7d3229967391a545470bd10044
NoManColonies <38200442+NoManColonies@users.noreply.github.com>
2020-06-17 14:07:14 +0700

    Add files via upload

--- form_update.php
+++ form_update.php
@@ -1,44 +1,44 @@
 <?php
-if (isset($_REQUEST['firstName'])) {
+if (!empty($_REQUEST['firstName'])) {
   $_SESSION['firstName'] = $_REQUEST['firstName'];
 }
-if (isset($_REQUEST['lastName'])) {
+if (!empty($_REQUEST['lastName'])) {
   $_SESSION['lastName'] = $_REQUEST['lastName'];
 }
-if (isset($_REQUEST['age'])) {
+if (!empty($_REQUEST['age'])) {
   $_SESSION['age'] = $_REQUEST['age'];
 }
-if (isset($_REQUEST['gender'])) {
+if (!empty($_REQUEST['gender'])) {
   $_SESSION['gender'] = $_REQUEST['gender'];
 }
-if (isset($_REQUEST['noDisease'])) {
+if (!empty($_REQUEST['noDisease'])) {
   $_SESSION['noDisease'] = $_REQUEST['noDisease'];
 }
-if (isset($_REQUEST['isHypertension'])) {
+if (!empty($_REQUEST['isHypertension'])) {
   $_SESSION['isHypertension'] = $_REQUEST['isHypertension'];
 }
-if (isset($_REQUEST['isDiabets'])) {
+if (!empty($_REQUEST['isDiabets'])) {
   $_SESSION['isDiabets'] = $_REQUEST['isDiabets'];
 }
-if (isset($_REQUEST['isDyslipidemia'])) {
+if (!empty($_REQUEST['isDyslipidemia'])) {
   $_SESSION['isDyslipidemia'] = $_REQUEST['isDyslipidemia'];
 }
-if (isset($_REQUEST['isHearth'])) {
+if (!empty($_REQUEST['isHearth'])) {
   $_SESSION['isHearth'] = $_REQUEST['isHearth'];
 }
-if (isset($_REQUEST['isRenopathy'])) {
+if (!empty($_REQUEST['isRenopathy'])) {
   $_SESSION['isRenopathy'] = $_REQUEST['isRenopathy'];
 }
-if (isset($_REQUEST['isPneumonopathy'])) {
+if (!empty($_REQUEST['isPneumonopathy'])) {
   $_SESSION['isPneumonopathy'] = $_REQUEST['isPneumonopathy'];
 }
-if (isset($_REQUEST['haveCough'])) {
+if (!empty($_REQUEST['haveCough'])) {
   $_SESSION['haveCough'] = $_REQUEST['haveCough'];
 }
-if (isset($_REQUEST['haveFever'])) {
+if (!empty($_REQUEST['haveFever'])) {
   $_SESSION['haveFever'] = $_REQUEST['haveFever'];
 }
-if (isset($_REQUEST['history'])) {
+if (!empty($_REQUEST['history'])) {
   $_SESSION['history'] = $_REQUEST['history'];
 }
 echo "done";
